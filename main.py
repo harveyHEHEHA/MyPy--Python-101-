87a9ebf702ba10e8e9bbe2662a7eb17213bf6194
Harvey Cobb <438013@edu.cwa.ac.uk>
2023-11-24 10:56:11 +0000

    changes made

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("My First Python Program")
+import datetime
+
+userIN = input("Enter your birth year: ")
+today = datetime.date.today()
+year = today.year
+result = year - int(userIN)
+print("Your birth year is", userIN, "making you", result, "years old.")
